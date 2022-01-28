5d7f3602c872f202de022453a874f843ffaced99
Sk Imtiaz Ahmed <imtiaz101325@gmail.com>
2022-01-28 11:15:09 +0600

    Added TOC

--- src/App.js
+++ src/App.js
@@ -8,6 +8,15 @@ function App() {
       <Typography variant="h4">
         A communication app for The Neighbourhood
       </Typography>
+      <br />
+      <Typography variant="h5">Terms of Service</Typography>
+      <Container>
+        <ul>
+          <li>
+            <Typography variant="body1">Just a bot for internal use</Typography>
+          </li>
+        </ul>
+      </Container>
     </Container>
   );
 }
