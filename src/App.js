cfb7868c5e18bb6e1f258da705d55ac79fc63514
Sk Imtiaz Ahmed <imtiaz101325@gmail.com>
2022-01-28 11:31:07 +0600

    Added privacy policy

--- src/App.js
+++ src/App.js
@@ -17,6 +17,15 @@ function App() {
           </li>
         </ul>
       </Container>
+      <br />
+      <Typography variant="h5">Privacy Policy</Typography>
+      <Container>
+        <ul>
+          <li>
+            <Typography variant="body1">No racism please</Typography>
+          </li>
+        </ul>
+      </Container>
     </Container>
   );
 }
